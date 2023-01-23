25b99bf3fe48d6cb42c45fea6158fe890f4403dc
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-01-23 15:33:11 +0100

    Function for wrongGuess and rightGuess done

--- hangman-assignment/js/hangman.js
+++ hangman-assignment/js/hangman.js
@@ -15,18 +15,40 @@
 
     .: poängräknare 
     .: tidräknare? om vi vill
-
-*/
-
+    
+    */
+   
 // Lista med ord
 const words = ['pizzamjöl', 'tomatsoppa', 'chorizogryta'];
 
+let correctGuesses = 0;
+let bodyParts = ['scaffold', 'head', 'body', 'arms', 'legs'];
+   
 // Funktion som tar fram ett slumpat index
 function getRandomIndex() {
     let randomIndex = Math.floor(Math.random() * words.length);
     return randomIndex;
 }
 
+// Funktion för att jämföra bokstav och rendera UI -- fixa så det bara görs
+// en gång!
+function rightGuess(letter) {
+
+    // Hämta de element med class name guessedLetter
+    let letterList = document.getElementsByClassName(letter);
+
+    // Loopa över listan för att skriva ut på skärmen
+    for (let i = 0; i < letterList.length; i++) {
+        letterList[i].innerHTML = letter.toUpperCase();
+    }
+}
+
+// Funktion för att rendera ut UI vid felgissning
+function wrongGuess() {
+
+    document.querySelector('figure').classList.add(bodyParts[0]);
+    bodyParts.shift();
+}
 
 // Hämta ord ur listan words med hjälp av random index
 let index = getRandomIndex();
@@ -45,18 +67,23 @@ for (let i = 0; i < wordToGuess.length; i++) {
     ulEl.appendChild(liEl);
 
 }
-let correctGuesses = 0;
+
 
 document.addEventListener('keypress', function(event) {
+    let letterExists = false;
     let guessedLetter = event.key;
 
     for (let i = 0; i < wordToGuess.length; i++) {
-        if(guessedLetter === wordToGuess[i]) {
-            console.log(guessedLetter);
+        if (guessedLetter === wordToGuess[i]) {
             correctGuesses++;
-        } else {
-            console.log('nä');
-        }
+                            // addera i argument
+            rightGuess(guessedLetter);
+            letterExists = true;
+        } 
+    }
+
+    if (!letterExists) {
+        wrongGuess();
     }
 
     if (correctGuesses === wordToGuess.length) {
